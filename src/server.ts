13ed64df0cdf6c0b2893565327c5e00e5c4e9539
TusksAndTasks <thedestroyerofworlds@mail.ru>
2022-06-19 23:47:17 +0300

    feat: add cluster functional

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import {createServer} from 'http';
 import { ApiControllers } from './controllers/apiControllers';
+import cluster from 'cluster';
 import { ResCodes } from './interfaces/enums';
 
 export const ApiController = new ApiControllers();
 
 export const server = createServer((req, res) => {
-
+        if(cluster.isWorker){console.log(`Worker ${cluster.worker?.id} handle request`);}
         if(req.url === '/api/users'){
             ApiController.workWithUsers(req, res);
         } else if ((req.url as string).match(/\/api\/users\/([0-z]+)/)) {
@@ -13,9 +14,11 @@ export const server = createServer((req, res) => {
         } else {
             ApiController.handleError(res, ResCodes.NOT_FOUND_CODE, ApiController.endpointError);         
         }
-    
 })
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}.`) 
+  if(cluster.worker){console.log(`Worker ${cluster.worker?.id} launched`)}
+})
